b3254e86c95dd5890bc1b8d3a338f816b93b9e4c
akshat mishra <lakshya764@gmail.com>
2023-08-08 13:01:49 +0530

    final push

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,16 +8,16 @@ import {
 const Footer = () => {
   return (
     <div className="py-5 mt-3  bg-amber-50 flex flex-wrap justify-center items-center gap-2 md:gap-10  absolute right-0 left-0 ">
-      <p>Eyesome made with 💜 by Sandhya </p>
+      <p>Eyesome made with 💜 by Lakshya </p>
       <p className="flex gap-3">
-        <a href="https://github.com/SandhyaR1007">
+        <a href="https://github.com/lakshnocode">
           <AiFillGithub className="text-2xl text-gray-800" />
         </a>
-        <a href="https://www.linkedin.com/in/sandhya-rajwanshi-a75b331b4/">
+        <a href="https://www.linkedin.com/in/lakshya-mishra-8ab9b0165/">
           {" "}
           <AiOutlineLinkedin className="text-2xl text-gray-800" />
         </a>
-        <a href="https://twitter.com/SandhyaR1007">
+        <a href="https://twitter.com/notarobbersol">
           <AiOutlineTwitter className="text-2xl text-gray-800" />
         </a>
       </p>
